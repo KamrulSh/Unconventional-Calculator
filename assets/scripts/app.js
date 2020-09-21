96c4b366e620dc3f19db80af0130b2ce849c28ca
Kamrul Islam Shahin <44506464+shahin-cuet@users.noreply.github.com>
2020-09-21 22:27:33 +0600

    remove repetative code to functions

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,36 +2,47 @@ const defaultResult =  0;
 let currentResult = defaultResult;
 let calculationDeccription;
 
+// get user inputed number
 function getUserInputedNumber() {
     return parseFloat(userInput.value);
 }
 
+// show the calculated output with log
+function getCalculatedOutput(operator, resultbeforeCalc, finalResult) {
+    calculationDeccription = resultbeforeCalc + operator + finalResult;
+    outputResult(currentResult, calculationDeccription);
+}
+
+// this function adds two numbers
 function addition() {
+    const prevCurrentResult = currentResult;
     const userInputedNumber = getUserInputedNumber();
-    calculationDeccription = currentResult + "+" + userInputedNumber;
     currentResult = currentResult + userInputedNumber;
-    outputResult(currentResult, calculationDeccription);
+    getCalculatedOutput("+", prevCurrentResult, userInputedNumber);
 }
 
+// this function subtracts two numbers
 function subtraction() {
+    const prevCurrentResult = currentResult;
     const userInputedNumber = getUserInputedNumber();
-    calculationDeccription = currentResult + "-" + userInputedNumber;
     currentResult = currentResult - userInputedNumber;
-    outputResult(currentResult, calculationDeccription);
+    getCalculatedOutput("-", prevCurrentResult, userInputedNumber);
 }
 
+// this function multiplies two numbers
 function multiplication() {
+    const prevCurrentResult = currentResult;
     const userInputedNumber = getUserInputedNumber();
-    calculationDeccription = currentResult + "*" + userInputedNumber;
     currentResult = currentResult * userInputedNumber;
-    outputResult(currentResult, calculationDeccription);
+    getCalculatedOutput("*", prevCurrentResult, userInputedNumber);
 }
 
+// this function devides two numbers
 function division() {
+    const prevCurrentResult = currentResult;
     const userInputedNumber = getUserInputedNumber();
-    calculationDeccription = currentResult + "/" + userInputedNumber;
     currentResult = currentResult / userInputedNumber;
-    outputResult(currentResult, calculationDeccription);
+    getCalculatedOutput("/", prevCurrentResult, userInputedNumber);
 }
 
 addBtn.addEventListener("click", addition);
